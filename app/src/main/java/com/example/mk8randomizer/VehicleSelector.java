d2fdd5e2073348596836873d701cdeeca805341c
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2022-05-01 13:32:10 +0200

    Added vehicle selector page TO DO : link this page to the others

---
+++ app/src/main/java/com/example/mk8randomizer/VehicleSelector.java
@@ -0,0 +1,14 @@
+package com.example.mk8randomizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_selector);
+    }
+}
